78c9a3d8c3f085fabd48b1a0c5015e03e9542dec
Michael Reuter <mareuter@lsst.org>
2022-06-17 15:01:37 -0700

    Adding startup/shutdown scripts.

---
+++ system_dev/startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+index=${1:-4}
+if [ $1 -eq 4 ]; then
+  config="--override=alliedvision.yaml"
+else
+  config=""
+fi
+python develop/csc_command.py --index=${index} start ${config}
+python develop/csc_command.py --index=${index} enable
+
