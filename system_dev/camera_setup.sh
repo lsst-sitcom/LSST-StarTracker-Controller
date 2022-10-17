8242635081bd2e31336e41b8334d53498cfee880
Michael Reuter <mareuter@lsst.org>
2022-10-17 15:50:23 -0700

    Update to new Vimba version.

--- system_dev/camera_setup.sh
+++ system_dev/camera_setup.sh
@@ -1,4 +1,4 @@
-vimba_version="Vimba_5_1"
+vimba_version="Vimba_6_0"
 export GENICAM_GENTL64_PATH=$GENICAM_GENTL64_PATH:"/home/saluser/develop/${vimba_version}/VimbaGigETL/CTI/x86_64bit"
 export PYTHONPATH=$PYTHONPATH:"/home/saluser/develop/${vimba_version}/VimbaPython/Source"
 
